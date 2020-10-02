53e6aa7ce76091bba44321f025fbe7d56c2bc08b
Georgi Yazmadzhian <gio6ko@gmail.com>
2020-10-02 23:21:26 +0300

    Triangle_area - C programe

---
+++ Triangle_area/cmake-build-debug/CMakeFiles/Triangle_area.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Triangle_area.dir/main.c.obj"
+  "Triangle_area.exe"
+  "Triangle_area.exe.manifest"
+  "Triangle_area.pdb"
+  "libTriangle_area.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Triangle_area.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
